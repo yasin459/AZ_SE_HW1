39e7d3a28c79bf58dc45859dc9e0c91b3d081f18
matinmoradi110 <matinmoradi110@gmail.com>
2023-07-12 23:36:58 +0330

    read lines from note.txt file

--- check_numbers.py
+++ check_numbers.py
@@ -1,6 +1,8 @@
 def check_numbers(filename):
-    pass
-
+    with open(filename, 'r') as file:
+        lines = file.readlines()    
+    lines = [line.strip().split() for line in lines]
+    
 filename = 'note.txt'
 are_numbers_same = check_numbers(filename)
 if are_numbers_same:
